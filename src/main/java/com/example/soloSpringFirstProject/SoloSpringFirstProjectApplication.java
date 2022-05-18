5f60160aa627311013354df7c6fe7bb783acbec8
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-05-18 15:52:55 +0100

    start of project

---
+++ src/main/java/com/example/soloSpringFirstProject/SoloSpringFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.soloSpringFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloSpringFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloSpringFirstProjectApplication.class, args);
+	}
+
+}
