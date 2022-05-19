ba0a3539cf8a21cfae5259d85ccefad0709064fa
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-05-19 09:15:27 +0100

    added get routines to owner

--- src/main/java/com/example/soloSpringFirstProject/controller/OwnerController.java
+++ src/main/java/com/example/soloSpringFirstProject/controller/OwnerController.java
@@ -1,8 +1,15 @@
 package com.example.soloSpringFirstProject.controller;
 
+import com.example.soloSpringFirstProject.model.Owner;
 import com.example.soloSpringFirstProject.respository.OwnerRepository;
 import com.example.soloSpringFirstProject.respository.VetRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class OwnerController {
@@ -12,4 +19,17 @@ public class OwnerController {
     public OwnerController(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
     }
+
+    @GetMapping("/owners")
+    public ResponseEntity<List<Owner>> getAllOwners() {
+        List<Owner> owners = ownerRepository.findAll();
+        return ResponseEntity.ok().body(owners);
+    }
+
+    @GetMapping("/owners/{id}")
+    public ResponseEntity<Optional<Owner>> getIndividualOwner(@PathVariable Long id) {
+        Optional<Owner> owner = ownerRepository.findById(id);
+        return ResponseEntity.ok().body(owner);
+    }
+
 }
